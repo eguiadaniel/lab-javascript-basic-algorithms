9d7c651589b618dfd68ecc26948c0f5d040fe039
eguiadaniel <40235952+eguiadaniel@users.noreply.github.com>
2020-10-27 09:28:54 +0100

    3 exercises completed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
-// Iteration 1: Names and Input
+/*Iteration 1: Names and Input
+1.1 Create a variable `hacker1` with the driver's name.
+1.2 Print `"The driver's name is XXXX"`.
+1.3 Create a variable `hacker2` with the navigator's name.
+1.4 Print `"The navigator's name is YYYY"`.
+*/ 
 
-const hacker1 = 'Joe';
-const hacker2 = 'Maximilian';
+const hacker1 = "Andrea";
+const hacker2 = "Zoe";
 
-console.log("The driver's name is " + hacker1);
-console.log("The navigator's name is " + hacker2);
+console.log('The driver\'s name is ' + hacker1,)
+console.log('The navigator\'s name is ' + hacker2,)
 
 /* Iteration 2: Conditionals
 2.1. Depending on which name is longer, print:
@@ -12,18 +17,13 @@ console.log("The navigator's name is " + hacker2);
 - It seems that the navigator has the longest name, it has XX characters. or
 - Wow, you both have equally long names, XX characters!.*/
 
-if (hacker1.length > hacker2.length) {
-  console.log(
-    `The driver has the longest name, it has ${hacker1.length} characters.`
-  );
-} else if (hacker1.length < hacker2.length) {
-  console.log(
-    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
-  );
+if ( hacker1.length > hacker2.length){
+console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+}
+else if ( hacker1.length < hacker2.length){
+console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 } else {
-  console.log(
-    `Wow, you both have equally long names,  ${hacker2.length} characters!`
-  );
+  console.log(`Wow, you both have equally long names,  ${hacker2.length} characters!`);
 }
 
 /* Iteration 3: Loops
@@ -41,11 +41,45 @@ TIP From Zé:
 `Hello` localCompare(`Help`)); -> Each word its translated to a number and soma are bigger than others
 
 */
-
 const hacker1UP = hacker1.toUpperCase();
-const hacker1SPLIT = hacker1UP.split('').join(' ');
-console.log(hacker1SPLIT);
+const hacker1SPLIT = hacker1UP.split("").join(" ");
+console.log(hacker1SPLIT)
 
-for (let i = hacker2.length; i >= 0; i--) {
-  console.log(hacker2[i]);
+console.log(hacker2.length)
+
+for (let i = hacker2.length ; i >= 0; i--){
+console.log(hacker2[i])
+}
+
+
+/*function reverseString(str) {
+    return str.split("").reverse().join("");
+    }
+reverseString(hacker2);
+
+console.log(str)
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("hello");
+*/
+
+    let string = hacker1;
+    var revString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        revString += string[i];
+    }
+    console.log(revString);
+
+
+
+if (hacker1 < hacker2) {
+  console.log("The driver's name goes first.");
+}
+if (hacker1 > hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+}
+if (hacker1 == hacker2) {
+  console.log("What?! You both have the same name?");
 }
